1f70fd32cb198666471f1a5b28d8f0febd835f32
Gustav-Gut <gustavo.gutierrezy@gmail.com>
2025-12-13 17:55:57 -0300

    [ADD] Validators for DTOs

--- src/students/dto/create-student.dto.ts
+++ src/students/dto/create-student.dto.ts
@@ -1,1 +1,19 @@
-export class CreateStudentDto {}
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreateStudentDto {
+    @IsString()
+    @IsNotEmpty()
+    firstName: string;
+
+    @IsString()
+    @IsNotEmpty()
+    lastName: string;
+
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
+
+    @IsString()
+    @IsOptional()
+    phone?: string;
+}
