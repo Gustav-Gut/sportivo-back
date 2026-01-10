fc4db1554dca94e4a77107d761220c495a28e34b
Gustav-Gut <gustavo.gutierrezy@gmail.com>
2026-01-10 13:07:58 -0300

    [RMV] PrismaModule is global, is not neccesary to invoke in every module

--- src/plans/plans.module.ts
+++ src/plans/plans.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PlansService } from './plans.service';
 import { PlansController } from './plans.controller';
-import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule],
   controllers: [PlansController],
   providers: [PlansService],
   exports: [PlansService],
