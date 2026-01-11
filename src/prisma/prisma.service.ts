5c679f18f7eb7d672166da0fcb31ecdfb1995ee8
Gustav-Gut <gustavo.gutierrezy@gmail.com>
2026-01-11 14:47:33 -0300

    [ADD] Prisma extension for respond user without password

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -5,11 +5,25 @@ import { PrismaPg } from '@prisma/adapter-pg';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
 
+    public readonly userWithoutPassword: any;
+
     constructor() {
         const adapter = new PrismaPg({
             connectionString: process.env.DATABASE_URL as string,
         });
         super({ adapter });
+
+        this.userWithoutPassword = this.$extends({
+            result: {
+                user: {
+                    password: {
+                        compute() {
+                            return undefined;
+                        },
+                    }
+                }
+            },
+        });
     }
 
     async onModuleInit() {
