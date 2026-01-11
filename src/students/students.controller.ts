83fe36dd556d1f4a8f14f08945f4ec60afbc015c
Gustav-Gut <gustavo.gutierrezy@gmail.com>
2026-01-11 14:48:34 -0300

    [CHG] Modify message for unathorized exception for schoolId header is required

--- src/students/students.controller.ts
+++ src/students/students.controller.ts
@@ -11,14 +11,14 @@ export class StudentsController {
   create(
     @Body() createStudentDto: CreateStudentDto,
     @Headers('x-school-id') schoolId: string) {
-    if (!schoolId) throw new UnauthorizedException('School ID is required header: x-school-id');
+    if (!schoolId) throw new UnauthorizedException('School ID is required header');
     return this.studentsService.create(createStudentDto, schoolId);
   }
 
   @Get()
   findAll(
     @Headers('x-school-id') schoolId: string) {
-    if (!schoolId) throw new UnauthorizedException('School ID is required header: x-school-id');
+    if (!schoolId) throw new UnauthorizedException('School ID is required header');
     return this.studentsService.findAll(schoolId);
   }
 
@@ -26,7 +26,7 @@ export class StudentsController {
   findOne(
     @Param('id') id: string,
     @Headers('x-school-id') schoolId: string) {
-    if (!schoolId) throw new UnauthorizedException('School ID is required header: x-school-id');
+    if (!schoolId) throw new UnauthorizedException('School ID is required header');
     return this.studentsService.findOne(id, schoolId);
   }
 
@@ -35,7 +35,7 @@ export class StudentsController {
     @Param('id') id: string,
     @Body() updateStudentDto: UpdateStudentDto,
     @Headers('x-school-id') schoolId: string) {
-    if (!schoolId) throw new UnauthorizedException('School ID is required header: x-school-id');
+    if (!schoolId) throw new UnauthorizedException('School ID is required header');
     return this.studentsService.update(id, updateStudentDto, schoolId);
   }
 
@@ -43,7 +43,7 @@ export class StudentsController {
   remove(
     @Param('id') id: string,
     @Headers('x-school-id') schoolId: string) {
-    if (!schoolId) throw new UnauthorizedException('School ID is required header: x-school-id');
+    if (!schoolId) throw new UnauthorizedException('School ID is required header');
     return this.studentsService.remove(id, schoolId);
   }
 }
