a4b31289b7dfdbd19bbc9287ba9cc5387a221450
Gustav-Gut <gustavo.gutierrezy@gmail.com>
2026-01-11 23:20:32 -0300

    [CHG] Plans endpoints now protected by ADMIN role

--- src/plans/plans.controller.ts
+++ src/plans/plans.controller.ts
@@ -2,11 +2,14 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, Headers, Unauthorize
 import { PlansService } from './plans.service';
 import { CreatePlanDto } from './dto/create-plan.dto';
 import { UpdatePlanDto } from './dto/update-plan.dto';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { Role } from '@prisma/client';
 
 @Controller('plans')
 export class PlansController {
   constructor(private readonly plansService: PlansService) { }
 
+  @Roles(Role.ADMIN)
   @Post()
   create(
     @Body() createPlanDto: CreatePlanDto,
@@ -15,11 +18,15 @@ export class PlansController {
     if (!schoolId) throw new UnauthorizedException('School ID is required');
     return this.plansService.create(schoolId, createPlanDto);
   }
-  @Get()
-  findAll(@Headers('x-school-id') schoolId: string) {
+
+  @Roles(Role.ADMIN)
+  findAll(
+    @Headers('x-school-id') schoolId: string) {
     if (!schoolId) throw new UnauthorizedException('School ID is required');
     return this.plansService.findAll(schoolId);
   }
+
+  @Roles(Role.ADMIN)
   @Get(':id')
   findOne(
     @Param('id') id: string,
@@ -28,6 +35,8 @@ export class PlansController {
     if (!schoolId) throw new UnauthorizedException('School ID is required');
     return this.plansService.findOne(id, schoolId);
   }
+
+  @Roles(Role.ADMIN)
   @Patch(':id')
   update(
     @Param('id') id: string,
@@ -37,6 +46,8 @@ export class PlansController {
     if (!schoolId) throw new UnauthorizedException('School ID is required');
     return this.plansService.update(id, schoolId, updatePlanDto);
   }
+
+  @Roles(Role.ADMIN)
   @Delete(':id')
   remove(
     @Param('id') id: string,
