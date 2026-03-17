03e9b37fb2d9cd4cdb256517162c17225de37880
Gustav-Gut <gustavo.gutierrezy@gmail.com>
2026-03-17 01:21:44 -0300

    [FIX] Now cookie is shared secure ibetween front and back in vercel production site.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -19,7 +19,7 @@ export class AuthController {
     res.cookie('access_token', access_token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'lax',
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       path: '/',
     });
 
